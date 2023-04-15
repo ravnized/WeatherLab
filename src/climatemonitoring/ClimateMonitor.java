1dc6886cc387836910583cc5c8063ea355aaa7d4
Gianpaolo Torino <43211190+ravnized@users.noreply.github.com>
2023-04-15 15:55:13 +0200

    initial setup for classes

---
+++ src/climatemonitoring/ClimateMonitor.java
@@ -0,0 +1,30 @@
+package src.climatemonitoring;
+
+public class ClimateMonitor {
+
+    boolean cercaAreaGeografica(){
+
+        return false;
+    }
+
+    boolean visualizzaAreaGeografica(){
+        return false;
+    }
+
+    boolean registrazione(){
+        return false;
+    }
+
+    boolean registraCentroAree(){
+        return false;
+    }
+
+    boolean inserisciParametriClimatici(){
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        System.out.println("Hello world");
+    }
+}
